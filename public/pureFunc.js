548fa0c97895ab08e3c5fd3c43be48a10039346a
EstrellaJYKIM <puris9030@gmail.com>
2018-09-12 06:32:25 +0900

    pureFunc

--- public/pureFunc.js
+++ public/pureFunc.js
@@ -14,12 +14,10 @@
         }
     }
 }
-    //복사하는 함수를 만들어라. 객체 안에 또 객체가 있어서 객체 안의 모든걸 전부 복사한다. 모든 객체를 복사할 수 있는 함수를 만들라.
+    //복사하는 함수를 만들어라. 객체 안에 또 객체가 있어서 객체 안의 모든걸 전부 복사한다.
+ // 0모든 객체를 복사할 수 있는 함수를 만들라.
 
-    var pure = function(p, myself){
-        var coopyObejct = Object.assign(p);
-        return copyOject;
-    }
+
     var arr = [a, b, c];
     var compose = function(arr1, arr2, arr3) {
         return function(arg){
@@ -28,6 +26,7 @@
          })
         }
     }
+
 
     var compose2 = function(func1, func2) {
         return func2(func2(arg));
